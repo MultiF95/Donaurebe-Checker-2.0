cc6914e6b0a455604f8f030e0abcaea10dcbeeaa
Florian Katholing <32741396+MultiF95@users.noreply.github.com>
2018-03-18 20:52:08 +0100

    Add files via upload

--- donau_main.sh
+++ donau_main.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# DONAUREBE CHECKER 3.0 -- FAVORITES AND NAME FILTER
 time=`date +"%H:%M"`
+
+#filter=`sed 's/,/ /g' | sed 's/,//g' | sed 's/RUNACC//g' | sed 's/runacc//g' | sed 's/petacc//g' | sed 's/longruner//g' | sed 's/Petacc//g'| sed 's/Sicai//g' | sed 's/Sable//g'`
 
 create () {
 curl -s http://47.91.95.44/index/show.html?page=1 > ~/donau/donau_sc.json
-jq '.data[] | .listingname' ~/donau/donau_sc.json | awk -F '"' '{print $2}' > ~/donau/donau_lo.txt
-awk '{print $1, $2, $3, $4}' ~/donau/donau_lo.txt > ~/donau/donau_pl.txt
+jq '.data[] | .listingname' ~/donau/donau_sc.json | awk -F '"' '{print $2}' | sed 's/,/ /g' | sed 's/,//g' > ~/donau/donau_lo.txt
+awk '{print $1, $2, $3, $4, $5}' ~/donau/donau_lo.txt > ~/donau/donau_pl.txt
 }
 
 createnow () {
 curl -s http://47.91.95.44/index/show.html?page=1 > ~/donau/donau_sc.json
-jq '.data[] | .listingname' ~/donau/donau_sc.json | awk -F '"' '{print $2}' > ~/donau/donau_lo.txt
-awk '{print $1, $2, $3, $4}' ~/donau/donau_lo.txt > ~/donau/donau_pl_tmp.txt
+jq '.data[] | .listingname' ~/donau/donau_sc.json | awk -F '"' '{print $2}' | sed 's/,/ /g' | sed 's/,//g' > ~/donau/donau_lo.txt
+awk '{print $1, $2, $3, $4, $5}' ~/donau/donau_lo.txt > ~/donau/donau_pl_tmp.txt
 }
-
-#main () {
 
 createnow
 
@@ -21,16 +22,18 @@ var1=`cat ~/donau/donau_pl.txt`
 var2=`cat ~/donau/donau_pl_tmp.txt`
 
 if [ ! "$var1" == "$var2" ]; then
-	
 	echo "check   @ $time"
+
 	diff -u ~/donau/donau_pl.txt ~/donau/donau_pl_tmp.txt | grep '^+' | awk -F'+' '{ print $2 }' | tail -n +2  > ~/donau/change.txt
+	 
 	list=`cat ~/donau/change.txt`
-	subject="Neue Produkte auf Donaurebe"
+	subject="NEW"
 	
 		if [ ! -z "$list" ]; then
 		
 		echo "update  @ $time"
 		
+		##osascript -e 'display notification "$list" with title "DonaurebeChecker"'
 		##PUSHBULLET
 		{
 		curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="donaurebe" -d body="$list" -d title="$subject" 'https://api.pushbullet.com/v2/pushes'
@@ -38,13 +41,19 @@ if [ ! "$var1" == "$var2" ]; then
 		##PUSHBULLET
 		
 		echo -e "\e[0m$list\e[0m"
+		
+			## Favorite
+			fav=`cat ~/donau/favorites.txt`
+			favlist=`grep "$fav" change.txt`
+			if [ ! -z "$favlist" ]; then
+				if [ ! "$favlist" == "$list" ]; then
+					curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="donaurebe" -d body="$favlist" -d title="FAVORITE" 'https://api.pushbullet.com/v2/pushes'			
+				fi
+			fi
+			##
 		create
 		fi
 
 else
 	echo "check   @ $time"
 fi
-#}
-
-#main
-#(echo >/dev/tcp/dcdle.net/22) &>/dev/null && echo "main server online. doing nothing" && exit || echo "other checker down. executing..." && main
