6aed254c2db0ae4d2667df791606e318a879da78
Florian Katholing <32741396+MultiF95@users.noreply.github.com>
2018-04-03 01:03:27 +0200

    Update rebecheck.sh

--- rebecheck.sh
+++ rebecheck.sh
@@ -1,80 +1,90 @@
 #!/bin/bash
-# DONAUREBE CHECKER 3.0 -- FAVORITES AND NAME FILTER
+#Rebechecker v4.0, Date: 03.04.2018
 time=`date +"%H:%M"`
 
-#filter=`sed 's/,/ /g' | sed 's/,//g' | sed 's/RUNACC//g' | sed 's/runacc//g' | sed 's/petacc//g' | sed 's/longruner//g' | sed 's/Petacc//g'| sed 's/Sicai//g' | sed 's/Sable//g'`
+#parameters:
+domain="47.91.95.44"
+tag="katholing" 				#pushbullet channel name
+min="50"						#minimum price to trigger expensive push
+path="/Users/katholing/rebe" 	#folder for temp files. Favour RAM Storage for 24/7 use
+endpage="3"			 			#json sites will be checked. Example: 03.04.2018 2 - 3 pages
+#key https://api.pushbullet.com/v2/pushes
+#api o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr
 
 create () {
-curl -o ~/donau/donau_sc.json -s http://47.91.95.44/index/fc.html?cond=price&ca=?
-#curl -s http://47.91.95.44/index/fc.html?page=2 > ~/donau/donau_sc2.json
-jq '.data[] | "\(.listingname) \(.price)"' ~/donau/donau_sc.json | awk -F '"' '{print $2}' > ~/donau/donau_lo.txt
-#jq '.data[] | "\(.listingname) \(.price)"' ~/donau/donau_sc2.json | awk -F '"' '{print $2}' > ~/donau/donau_lo2.txt
-awk '{print ">",$1, $2, $3, $4, $5, "||", $NF,"€"}' ~/donau/donau_lo.txt | sed 's/,//g' > ~/donau/donau_pl.txt
-#awk '{print ">",$1, $2, $3, $4, $5, "||", $NF,"€"}' ~/donau/donau_lo2.txt | sed 's/,//g' >> ~/donau/donau_pl.txt
-#awk '$0="*"$0' donau_plu.txt > donau_pl.txt
-#price###
-#########
+rm $path/plu$state.txt
+for (( page = 1; page <= $endpage; page++ ))
+do
+	echo get page $page json
+	curl -s http://$domain/index/fc.html?page=$page > $path/source$page.json
+	jq '.data[] | "\(.listingname) \(.price)"' $path/source$page.json | awk -F '"' '{print $2}' >> $path/plu$state.txt
+	sleep 1.5
+done
+awk '{print ">",$1, $2, $3, $4, $5, "||", $NF,"€"}' $path/plu$state.txt | sed 's/,//g' > $path/pl$state.txt
+#price
+echo downloading price json
+curl -s http://47.91.95.44/index/fc.html?cond=price > $path/sourcep.json
+sleep 1.5
+jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' | head -n 1| awk '{print $NF}' | awk -F "." '{print $1}' > $path/price$state.txt
 }
 
-createnow () {
-curl -o ~/donau/donau_sc.json -s http://47.91.95.44/index/fc.html?cond=price&ca=?
-#curl -s http://47.91.95.44/index/fc.html?page=2 > ~/donau/donau_sc2.json
-jq '.data[] | "\(.listingname) \(.price)"' ~/donau/donau_sc.json | awk -F '"' '{print $2}' > ~/donau/donau_lo_tmp.txt
-#jq '.data[] | "\(.listingname) \(.price)"' ~/donau/donau_sc2.json | awk -F '"' '{print $2}' > ~/donau/donau_lo2_tmp.txt
-awk '{print ">",$1, $2, $3, $4, $5, "||", $NF,"€"}' ~/donau/donau_lo_tmp.txt | sed 's/,//g' > ~/donau/donau_pl_tmp.txt
-#awk '{print ">",$1, $2, $3, $4, $5, "||", $NF,"€"}' ~/donau/donau_lo2_tmp.txt | sed 's/,//g' >> ~/donau/donau_pl_tmp.txt
-#awk '$0="*"$0' donau_plu_tmp.txt > donau_pl_tmp.txt
+state="_tmp"
+create
 
-#price#######
-#############
-}
+pl=`cat $path/pl.txt`
+plt=`cat $path/pl_tmp.txt`
 
-createnow
-
-var1=`cat ~/donau/donau_pl.txt`
-var2=`cat ~/donau/donau_pl_tmp.txt`
-
-if [ "$var1" != "$var2" ]; then
-	echo "check   @ $time"
-	diff -u ~/donau/donau_pl.txt ~/donau/donau_pl_tmp.txt | grep '^+' | awk -F'+' '{ print $2 }' | tail -n +2  > ~/donau/change.txt
-	list=`cat ~/donau/change.txt`
-	subject="NEW"
-	
-		if [ ! -z "$list" ]; then
-		##EXPENSIVE CHECK
-		pr=`cat ~/donau/change.txt | head -n 1 | awk '{print $(NF-1)}' | awk -F "." '{print $1}'`
-		if [ "$pr" -ge 50 ]; then
-			json=`curl -s http://47.91.95.44/index/fc.html?cond=price&ca=&page=1`
-			echo $json > ~/donau/donau_sc_price.json
-			id=`cat ~/donau/donau_sc_price.json | awk -F '"' '{print $10}'`
-			na=`cat ~/donau/change.txt | awk '{print $1, $2, $3, $4, $5, " für "}'`
-			tit=`echo "EXPENSIVE "$na $pr "€"`
-			li="http://47.91.95.44/index/product.html?orderid=$id"
-			{
-			curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="donaurebe" -d body="$li" -d title="$tit" 'https://api.pushbullet.com/v2/pushes'
-			} &> /dev/null
-			echo "EXPENSIVE: "$na $pr "€"
-		fi
-		##
-		echo "update  @ $time"
-		##PUSHBULLET
-		{
-		curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="donaurebe" -d body="$list" -d title="$subject" 'https://api.pushbullet.com/v2/pushes'
-		} &> /dev/null
+if [ "$pl" != "$plt" ]; then
 		
-		##PUSHBULLET
-		echo -e "\e[0m$list\e[0m"
-			## Favorite
-			fav=`cat ~/donau/favorites.txt`
-			favlist=`grep "$fav" ~/donau/change.txt`
-			if [ ! -z "$favlist" ]; then
-				if [ "$favlist" != "$list" ]; then
-				curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="donaurebe" -d body="$favlist" -d title="FAVORITE" 'https://api.pushbullet.com/v2/pushes'			
-				fi
+		##LIST UPDATE
+		echo "productlist update"
+		diff -u $path/pl.txt $path/pl_tmp.txt | grep '^+' | awk -F'+' '{ print $2 }' | tail -n +2 > $path/change.txt
+		list=`cat $path/change.txt`
+		
+		##TRIGGER IF UPDATE NOT NULL
+		if [ ! -z "$list" ]; then
+		echo "changes checked and set"
+		
+		##PRICE CHECK
+		p=`cat $path/price.txt`
+		ptmp=`cat $path/price_tmp.txt`
+		
+			if [ "$ptmp" -ge "$min" -a "$ptmp" -ne "$p" ]; then
+				id=`cat $path/sourcep.json | awk -F '"' '{print $10}'`
+				link=`echo http://$domain/index/product.html?orderid=$id`
+				na=`jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' | head -n 1| awk '{print $1, $2, $3, $4, $5}'`
+				pricetitle=`echo "EXPENSIVE "$na $ptmp "€"`
+				{
+				curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$link" -d title="$pricetitle" 'https://api.pushbullet.com/v2/pushes' 
+				} &> /dev/null
+				echo $ptmp > $path/price.txt
 			fi
-			##
+
+		##PUSH
+		{
+		curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$list" -d title="NEW" 'https://api.pushbullet.com/v2/pushes'
+		} &> /dev/null
+
+		##ECHO
+		echo "updates:"
+		echo "$list"
+
+		##FAVORITES
+		fav=`cat favorites.txt`
+		favlist=`grep "$fav" $path/change.txt`
+		if [ ! -z "$favlist" ]; then
+			if [ "$favlist" != "$list" ]; then
+			curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$favlist" -d title="FAVORITE" 'https://api.pushbullet.com/v2/pushes'			
+			fi
+		fi
+
+		##CREATE
+		state=""
 		create
+
 		fi
 else
-	echo "check   @ $time"
+
+	echo "no update"
+
 fi
