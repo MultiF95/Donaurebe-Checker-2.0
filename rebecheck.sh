b9ee23e308739ef39e9f3de849623d7a38df8882
Florian Katholing <32741396+MultiF95@users.noreply.github.com>
2018-04-04 18:05:30 +0200

    Update rebecheck.sh

--- rebecheck.sh
+++ rebecheck.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-#Rebechecker v4.0, Date: 03.04.2018
-
+#Rebechecker v4.1, Date: 03.04.2018
 time=`date +"%H:%M"`
 
 #parameters:
 domain="47.91.95.44"
-tag="" 							#pushbullet channel name
+tag="katholing" 				#pushbullet channel name
 min="50"						#minimum price to trigger expensive push
-path="" 						#folder for temp files. Favour RAM Storage for 24/7 use
+path="/Users/katholing/rebe" 	#folder for temp files. Favour RAM Storage for 24/7 use
 endpage="3"			 			#json sites will be checked. Example: 03.04.2018 2 - 3 pages
+
+#key https://api.pushbullet.com/v2/pushes
+#api o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr
 
 create () {
 rm $path/plu$state.txt
@@ -19,12 +21,13 @@ do
 	jq '.data[] | "\(.listingname) \(.price)"' $path/source$page.json | awk -F '"' '{print $2}' >> $path/plu$state.txt
 	sleep 1.5
 done
-awk '{print ">",$1, $2, $3, $4, $5, "||", $NF,"€"}' $path/plu$state.txt | sed 's/,//g' > $path/pl$state.txt
 #price
 echo downloading price json
 curl -s http://$domain/index/fc.html?cond=price > $path/sourcep.json
 sleep 1.5
+jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' >> $path/plu$state.txt
 jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' | head -n 1| awk '{print $NF}' | awk -F "." '{print $1}' > $path/price$state.txt
+sort -u $path/plu$state.txt | awk '{print ">",$1, $2, $3, $4, $5, "||", $NF,"€"}' | sed 's/,//g' > $path/pl$state.txt
 }
 
 state="_tmp"
@@ -54,14 +57,14 @@ if [ "$pl" != "$plt" ]; then
 				na=`jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' | head -n 1| awk '{print $1, $2, $3, $4, $5}'`
 				pricetitle=`echo "EXPENSIVE "$na $ptmp "€"`
 				{
-				curl -s -u """>apikeyhere<"":" -d type="note" -d channel_tag="$tag" -d body="$link" -d title="$pricetitle" 'https://api.pushbullet.com/v2/pushes' 
+				curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$link" -d title="$pricetitle" 'https://api.pushbullet.com/v2/pushes' 
 				} &> /dev/null
 				echo $ptmp > $path/price.txt
 			fi
 
 		##PUSH
 		{
-		curl -s -u """>apikeyhere<"":" -d type="note" -d channel_tag="$tag" -d body="$list" -d title="NEW" 'https://api.pushbullet.com/v2/pushes'
+		curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$list" -d title="NEW" 'https://api.pushbullet.com/v2/pushes'
 		} &> /dev/null
 
 		##ECHO
@@ -73,7 +76,7 @@ if [ "$pl" != "$plt" ]; then
 		favlist=`grep "$fav" $path/change.txt`
 		if [ ! -z "$favlist" ]; then
 			if [ "$favlist" != "$list" ]; then
-			curl -s -u """>apikeyhere<"":" -d type="note" -d channel_tag="$tag" -d body="$favlist" -d title="FAVORITE" 'https://api.pushbullet.com/v2/pushes'			
+			curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$favlist" -d title="FAVORITE" 'https://api.pushbullet.com/v2/pushes'			
 			fi
 		fi
 
