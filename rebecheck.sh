b7fd674bdfeebbe2ece6e8d1b252c029e61e7419
Florian Katholing <32741396+MultiF95@users.noreply.github.com>
2018-08-04 21:33:56 +0200

    Update rebecheck.sh

--- rebecheck.sh
+++ rebecheck.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-#Rebechecker v4.1, Date: 04.04.2018
+#Rebechecker v4.2, Date: 04.08.2018
 time=`date +"%H:%M"`
 
 #parameters:
 domain="47.91.95.44"
-tag="" #pushbullet channel name
-min="50" #minimum price to trigger expensive push
-path="/Users/xxx/xxx" #folder for temp files. Favour RAM Storage for 24/7 use
-endpage="3" #json sites will be checked. Example: 03.04.2018 2 - 3 pages
+tag="donaurebe" 				#pushbullet channel name
+min="50"						#minimum price to trigger expensive push
+path="/home/pi/donau" 	#folder for temp files. Favour RAM Storage for 24/7 use
+endpage="3"			 			#json sites will be checked. Example: 03.04.2018 2 - 3 pages
+
+#key https://api.pushbullet.com/v2/pushes
+#api o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr
 
 create () {
 rm $path/plu$state.txt
 for (( page = 1; page <= $endpage; page++ ))
 do
 	echo downloading page $page json
-	curl -s http://$domain/index/fc.html?page=$page > $path/source$page.json
+	curl -k https://$domain/index/show.html?page=$page > $path/source$page.json
 	jq '.data[] | "\(.listingname) \(.price)"' $path/source$page.json | awk -F '"' '{print $2}' >> $path/plu$state.txt
 	sleep 1.5
 done
 #price
 echo downloading price json
-curl -s http://$domain/index/fc.html?cond=price > $path/sourcep.json
+curl -k https://$domain/index/fc.html?cond=price > $path/sourcep.json
 sleep 1.5
 jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' >> $path/plu$state.txt
 jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' | head -n 1| awk '{print $NF}' | awk -F "." '{print $1}' > $path/price$state.txt
@@ -54,14 +57,14 @@ if [ "$pl" != "$plt" ]; then
 				na=`jq '.data[] | "\(.listingname) \(.price)"' $path/sourcep.json | awk -F '"' '{print $2}' | head -n 1| awk '{print $1, $2, $3, $4, $5}'`
 				pricetitle=`echo "EXPENSIVE "$na $ptmp "€"`
 				{
-				curl -s -u """>apikeyhere<"":" -d type="note" -d channel_tag="$tag" -d body="$link" -d title="$pricetitle" 'https://api.pushbullet.com/v2/pushes' 
+				curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$link" -d title="$pricetitle" 'https://api.pushbullet.com/v2/pushes' 
 				} &> /dev/null
 				echo $ptmp > $path/price.txt
 			fi
 
 		##PUSH
 		{
-		curl -s -u """>apikeyhere<"":" -d type="note" -d channel_tag="$tag" -d body="$list" -d title="NEW" 'https://api.pushbullet.com/v2/pushes'
+		curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$list" -d title="NEW" 'https://api.pushbullet.com/v2/pushes'
 		} &> /dev/null
 
 		##ECHO
@@ -73,7 +76,7 @@ if [ "$pl" != "$plt" ]; then
 		favlist=`grep "$fav" $path/change.txt`
 		if [ ! -z "$favlist" ]; then
 			if [ "$favlist" != "$list" ]; then
-			curl -s -u """>apikeyhere<"":" -d type="note" -d channel_tag="$tag" -d body="$favlist" -d title="FAVORITE" 'https://api.pushbullet.com/v2/pushes'			
+			curl -s -u """o.l01W0uErXooJuf0DZhHXsHXlXnij9oEr"":" -d type="note" -d channel_tag="$tag" -d body="$favlist" -d title="FAVORITE" 'https://api.pushbullet.com/v2/pushes'			
 			fi
 		fi
 
